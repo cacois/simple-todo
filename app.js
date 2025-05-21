1a5126d073b8a92d8b95377f236778fccc1a4887
Constantine Aaron Cois <cacois@gmail.com>
2025-05-21 09:04:10 -0400

    cleanup

--- app.js
+++ app.js
@@ -22,14 +22,16 @@ const dbRootPass = process.env.DB_ROOT_PASS
   ? process.env.DB_ROOT_PASS
   : "my-secret-root-pw";
 
-const app = express();
-app.use(bodyParser.json());
+// global DB connection object
 let db;
 
+const app = express();
+app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.static('node_modules/todomvc-common'));
 app.use(express.static('node_modules/todomvc-app-css'));
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 
 /**
  * Creates the database if it doesn't exist
@@ -37,7 +39,7 @@ app.use(favicon(__dirname + '/public/images/favicon.ico'));
 async function initDB() {
   try {
     // now init non-root connection to use
-    console.log('connecting to mysql...');
+    console.log('connecting to database...');
     db = await mysql.createConnection({host: dbHost, user: dbUser, password: dbPass, database: databaseName});
   } catch (e) {
     console.log(`ERROR: Unable to connect to database: ${e}`);
@@ -45,20 +47,25 @@ async function initDB() {
   }
 
   try {
-    console.log('creating todos table...');
+    console.log('creating database tables...');
     const createTableQuery = 
       "CREATE TABLE IF NOT EXISTS todos (id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL, complete BOOLEAN NOT NULL)";
     await db.query(createTableQuery);
   } catch (e) {
-    console.log(`ERROR: Create table if not exists query failed: ${e}`);
+    console.log(`ERROR: Create table query failed: ${e}`);
     throw e;
   }
   console.log("done")
 }
 
+/**
+ * Root
+ */
 app.get("/", (req, res) => res.render("index.html"));
-app.get("/favicon.ico", (req, res) => res.render("favicon.ico"));
 
+/**
+ * Get all todos in database
+ */
 app.get("/todos", async function (req, res) {
   console.log('GET /todos')
 
@@ -86,7 +93,6 @@ app.get("/todos", async function (req, res) {
  * todo structure: {"id": #, "text": string, "complete": boolean}
  */
 app.put("/todos", async function (req, res) {
-  console.log('writing todos');
   
   // if we detect a lack of connection, try to connect
   if(!db || !db.connection) {
@@ -95,9 +101,8 @@ app.put("/todos", async function (req, res) {
   }
 
   if (db && db.connection) {
-    console.log('clearing table...');
     try {
-      [results] = await db.query("DELETE FROM todos");
+      await db.query("DELETE FROM todos");
     } catch (err) {
       console.log(`error on delete: ${err}`)
       res.status(500).send();
@@ -106,25 +111,18 @@ app.put("/todos", async function (req, res) {
 
     errors = [];
     todos = req.body
-    console.log('inserting...');
-
     // format todos into nested array for sql bulk insert
     todos = todos.map((todo) => [todo.text, todo.complete])
 
     try {
       // format bulk insert query string
       const query = mysql.format("INSERT INTO todos (text, complete) VALUES ?", [todos])
-
-      console.log(`query: ${query}`)
       await db.execute(query);
 
-      console.log('todos synced with db');
     } catch (err) {
       console.log(`Insert error: ${err}`);
       errors.push(err);
     }
-
-    console.log(`errors.length: ${errors.length}`)
 
     if (errors.length > 0) {
       console.log('500')
@@ -137,7 +135,9 @@ app.put("/todos", async function (req, res) {
 });
 
 
-// Main: init DB
+/**
+ * Main program entrypoint
+ */
 try {
   initDB();
 } catch (e) {
